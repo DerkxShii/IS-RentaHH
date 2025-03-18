4937cf5b69e52a11adfce22f1f292d07dc9c8e75
Derko <derkovd25@gmail.com>
2025-03-18 09:39:39 -0600

    Update: .

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -45,39 +45,6 @@ const services = [
   },
 ];
 
-const solutions = [
-  {
-    title: "Visibility Dilemma",
-    painPoint:
-      "Your brand is drowning in the vast sea of social media, struggling to be seen by your target audience.",
-    agitatepainPoint:
-      "It's disheartening when your carefully crafted content goes unnoticed. Your potential customers are out there, but they simply can't find you amidst the digital noise.",
-    solution:
-      "Our targeted visibility strategies cut through the clutter, ensuring your brand is not just seen but remembered. Watch our video to see how we make your presence impossible to ignore.",
-    img: visibilityDilemma,
-  },
-  {
-    title: "Engagement Gap",
-    painPoint:
-      "Despite your efforts, audience engagement remains lackluster, and interactions are not translating into meaningful connections.",
-    agitatepainPoint:
-      "It's frustrating to see competitors effortlessly engaging their audience while your brand struggles to spark conversations. Your content deserves better results, and we have the solution.",
-    solution:
-      "Our proven engagement tactics ignite conversations, turning passive viewers into active participants. Explore how we bridge the engagement gap in our video walkthrough.",
-    img: engagementGap,
-  },
-  {
-    title: "Results in Hibernation",
-    painPoint:
-      "Your social media efforts aren't translating into tangible results, leaving you wondering if your investment is paying off.",
-    agitatepainPoint:
-      "It's disheartening when the metrics show little growth or impact. Your time and resources are valuable, and it's time to break free from the cycle of stagnant results.",
-    solution:
-      "Our data-driven approach ensures every action is purposeful, yielding measurable results. Dive into our video guide to witness how we turn social media activities into a powerful engine for your brand's success.",
-    img: resultsInHibernation,
-  },
-];
-
 const faqs = [
   {
     question: "¿El plan de renta incluye software?",
@@ -100,4 +67,4 @@ const faqs = [
   },
 ];
 
-export { services, solutions, faqs };
+export { services, faqs };
